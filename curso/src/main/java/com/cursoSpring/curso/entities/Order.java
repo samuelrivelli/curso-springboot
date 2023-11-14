7ca6d4ea1545428909ba1fba05a0bd7d729f3f37
Samuel Rivelli <samuelmagrivelli@gmail.com>
2023-11-14 08:05:08 -0300

    Associao one to one para payment

--- curso/src/main/java/com/cursoSpring/curso/entities/Order.java
+++ curso/src/main/java/com/cursoSpring/curso/entities/Order.java
@@ -31,6 +31,9 @@ public class Order implements Serializable {
 
     private Integer orderStatus;
 
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
+
     public Order() {
     }
 
@@ -76,6 +79,14 @@ public class Order implements Serializable {
 
     public Set<OrderItem> getItems(){
         return items;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
     @Override
